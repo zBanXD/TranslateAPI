c11c0a3404e4e808b574e6be22ba88a088e13b7f
zBanXD <zFuzion538@gmail.com>
2019-04-04 14:42:48 -0300

    Add files via upload

---
+++ src/zBanXD/TranslateAPI.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+namespace zBanXD;
+
+use TranslateAPI\GoogleTranslate;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+
+class Main extends PluginBase {
+
+    public function onEnable(): void
+    {
+        Server::getInstance()->getLogger()->info("§ TranslateAPI § Ativado e funcional!");
+    }
+
+    /**
+     * Retorna instancia para poder ser implementado projetos externos.
+     */
+    public function getInstance()
+    {
+        return $this;
+    }
+
+    /**
+     * Função para receber mensagens (strings) e retornar mensagens (strings) traduzidas.
+     */
+    public function translate(string $message, string $original, string $target)
+    {
+        $tr = new GoogleTranslate();
+        $tr->setSource($original);
+        $tr->setTarget($target);
+        return $tr->translate($message);
+    }
+
+}
